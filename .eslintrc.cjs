984408b2297a226b6d22de2cc83bb6fa6fed56ab
haram8009 <haram8009@gmail.com>
2024-07-08 21:00:27 +0900

    feat: husky로 git hooks 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,13 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:import/recommended',
     'plugin:jsx-a11y/recommended',
+    'airbnb',
+    'airbnb/hooks',
     // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
     // Make sure it's always the last config, so it gets the chance to override other configs.
     'eslint-config-prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.js'], //module 에러 나는 파일들을 예외처리해줌
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: {
     react: { version: '18.2' },
@@ -24,9 +26,14 @@ module.exports = {
       },
     },
   },
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/prop-types': 'off',
+    'import/no-unresolved': 'off',
+    'no-alert': 'off',
+    'no-console': 'off',
+    'react/button-has-type': 'off',
   },
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import theme from '@styles/globalTheam.js';
+import theme from '@styles/globalTheam';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={theme}>
