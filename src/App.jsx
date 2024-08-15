1bb6b0603d28a4cae98b6f044343b067c4d6cc36
godeka <godeka@naver.com>
2024-08-15 16:12:01 +0900

    feat: 시작화면에서 로그인/회원가입 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -33,10 +33,10 @@ function FirstPage() {
         <Typography variant="h4" align="center" sx={{ mt: 5, mb: 30 }}>
           Academy Pro
         </Typography>
-        <Button variant="contained" size="large" sx={{ m: 1 }} fullWidth>
+        <Button variant="contained" size="large" sx={{ m: 1 }} fullWidth href="/login">
           로그인
         </Button>
-        <Button variant="contained" size="large" sx={{ m: 1 }} fullWidth>
+        <Button variant="contained" size="large" sx={{ m: 1 }} fullWidth href="/signup">
           회원가입
         </Button>
       </Box>
