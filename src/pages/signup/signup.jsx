1028af64c7784a89637b804c651bf7f6e936eb65
godeka <godeka@naver.com>
2024-08-18 18:44:47 +0900

    feat: 회원가입 성공 화면

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -8,11 +8,12 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 export default function SignUp() {
-  const [selected, setSelected] = useState(false);
+  // 0: 선택화면, 1: 회원가입화면 2: 완료화면
+  const [status, setStatus] = useState(0);
   const [role, setRole] = useState('');
 
   const handleClickRole = (_role) => {
-    setSelected(true);
+    setStatus(1);
     setRole(_role);
   };
 
@@ -29,19 +30,9 @@ export default function SignUp() {
         <Typography variant="h4" align="center" mb={5}>
           Academy Pro
         </Typography>
-        <Box mb={10}>
-          <Typography variant="h5" align="center" mb={1}>
-            회원가입
-          </Typography>
-          {selected && (
-            <Typography variant="body1" align="center">
-              {role === 'director' && '학원 대표'}
-              {role === 'teacher' && '학원 강사'}
-              {role === 'student' && '학생/학부모'}
-            </Typography>
-          )}
-        </Box>
-        {selected ? <SignupForm role={role} setSelected={setSelected} /> : <RoleSelection handleClickRole={handleClickRole} />}
+        {status === 0 && <RoleSelection handleClickRole={handleClickRole} />}
+        {status === 1 && <SignupForm role={role} setStatus={setStatus} />}
+        {status === 2 && <Succeed name="홍길동" role={role} />}
       </Box>
     </Container>
   );
@@ -49,7 +40,10 @@ export default function SignUp() {
 
 function RoleSelection({ handleClickRole }) {
   return (
-    <Box mt={20}>
+    <Box>
+      <Typography variant="h5" align="center" mb={30}>
+        회원가입
+      </Typography>
       <Button variant="contained" size="large" sx={{ m: 1 }} fullWidth onClick={() => handleClickRole('director')}>
         학원 대표
       </Button>
@@ -70,7 +64,7 @@ function RoleSelection({ handleClickRole }) {
   );
 }
 
-function SignupForm({ role, setSelected }) {
+function SignupForm({ role, setStatus }) {
   const [showPassword, setShowPassword] = useState(false);
 
   const handleClick = () => {
@@ -88,71 +82,84 @@ function SignupForm({ role, setSelected }) {
     };
 
     if (submitData.password !== submitData.password2) alert('입력한 두 비밀번호가 일치하지 않습니다.');
-    else console.log(submitData);
+    else {
+      console.log(submitData);
+      setStatus(2);
+    }
   };
 
   return (
-    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
-          <TextField name="userid" id="userid" label="아이디" required fullWidth autoFocus />
+    <Box>
+      <Typography variant="h5" align="center" mb={1}>
+        회원가입
+      </Typography>
+      <Typography variant="body1" align="center" mb={10}>
+        {role === 'director' && '학원 대표'}
+        {role === 'teacher' && '학원 강사'}
+        {role === 'student' && '학생/학부모'}
+      </Typography>
+      <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <TextField name="userid" id="userid" label="아이디" required fullWidth autoFocus />
+          </Grid>
+          <Grid item xs={4}>
+            <Button variant="contained" size="large" sx={{ m: 1 }}>
+              중복확인
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleClick} edge="end">
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              required
+              fullWidth
+              name="password"
+              label="비밀번호"
+              type={showPassword ? 'text' : 'password'}
+              id="password"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleClick} edge="end">
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              type={showPassword ? 'text' : 'password'}
+              required
+              fullWidth
+              name="password2"
+              label="비밀번호 확인"
+              id="password2"
+            />
+          </Grid>
+          {role === 'student' ? <PersonalInfo title="학생 정보" /> : <PersonalInfo title="개인 정보" />}
+          {role === 'student' && <PersonalInfo title="학부모 정보" />}
         </Grid>
-        <Grid item xs={4}>
-          <Button variant="contained" size="large" sx={{ m: 1 }}>
-            중복확인
-          </Button>
+        <Button type="submit" fullWidth variant="contained" size="large" sx={{ mt: 3 }}>
+          가입하기
+        </Button>
+        <Grid container justifyContent="flex-end" sx={{ mt: 1, mb: 3 }}>
+          <Grid item>
+            <Button variant="text" onClick={() => setStatus(0)}>
+              이전 화면으로
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <TextField
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={handleClick} edge="end">
-                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-            required
-            fullWidth
-            name="password"
-            label="비밀번호"
-            type={showPassword ? 'text' : 'password'}
-            id="password"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={handleClick} edge="end">
-                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-            type={showPassword ? 'text' : 'password'}
-            required
-            fullWidth
-            name="password2"
-            label="비밀번호 확인"
-            id="password2"
-          />
-        </Grid>
-        {role === 'student' ? <PersonalInfo title="학생 정보" /> : <PersonalInfo title="개인 정보" />}
-        {role === 'student' && <PersonalInfo title="학부모 정보" />}
-      </Grid>
-      <Button type="submit" fullWidth variant="contained" size="large" sx={{ mt: 3 }}>
-        가입하기
-      </Button>
-      <Grid container justifyContent="flex-end" sx={{ mt: 1, mb: 3 }}>
-        <Grid item>
-          <Button variant="text" onClick={() => setSelected(false)}>
-            이전 화면으로
-          </Button>
-        </Grid>
-      </Grid>
+      </Box>
     </Box>
   );
 }
@@ -184,5 +191,22 @@ function PersonalInfo({ title }) {
         <TextField required fullWidth id="phonenumber" label="전화번호" name="phonenumber" />
       </Grid>
     </>
+  );
+}
+
+function Succeed({ name, role }) {
+  return (
+    <Box>
+      <Typography variant="h5" align="center" mb={20}>
+        {name}
+        {role === 'director' && '(원장)'}
+        {role === 'teacher' && '(강사)'}
+        님, <br />
+        회원가입이 완료되었습니다!
+      </Typography>
+      <Button variant="contained" size="large" fullWidth href="/login">
+        로그인
+      </Button>
+    </Box>
   );
 }
