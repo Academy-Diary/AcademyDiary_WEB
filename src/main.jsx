e6b90ebbfdb81ea469d4b155eeadd758cc08ead7
haram8009 <haram8009@gmail.com>
2024-08-19 04:35:43 +0900

    feat: custom designed scroll-bar

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from '@styles/globalTheme';
+import '@styles/globalStyles.css';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
