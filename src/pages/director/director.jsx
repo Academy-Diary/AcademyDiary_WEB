301ca6014815238c06476de0d24f619489610401
godeka <godeka@naver.com>
2024-08-20 00:15:35 +0900

    feat: 마우스오버 메뉴 추가 #8

--- src/pages/director/director.jsx
+++ src/pages/director/director.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Box, Typography, AppBar, Toolbar, Button, IconButton, Avatar } from '@mui/material';
+import { Box, Typography, AppBar, Toolbar, Button, IconButton, Avatar, Menu, MenuItem } from '@mui/material';
 
 export default function Director() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorEl2, setAnchorEl2] = useState(null);
+
+  const handleMouseOver = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleMouseOver2 = (e) => {
+    setAnchorEl2(e.currentTarget);
+  };
+  const handleClose2 = () => {
+    setAnchorEl2(null);
+  };
+
   return (
     <Box component="main">
       <AppBar position="static">
@@ -11,15 +27,25 @@ export default function Director() {
             AcademyPro
           </Typography>
           <Box sx={{ flexGrow: 1, ml: 5 }}>
-            <Button color="inherit" size="large" sx={{ mx: 2 }}>
+            <Button color="inherit" size="large" sx={{ mx: 2 }} onMouseOver={handleMouseOver}>
               구성원 관리
             </Button>
+            <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+              <MenuItem onClick={handleClose}>등록 요청 목록</MenuItem>
+              <MenuItem onClick={handleClose}>강사 목록</MenuItem>
+              <MenuItem onClick={handleClose}>학생 목록</MenuItem>
+            </Menu>
             <Button color="inherit" size="large" sx={{ mx: 2 }}>
               강의 관리
             </Button>
-            <Button color="inherit" size="large" sx={{ mx: 2 }}>
+            <Button color="inherit" size="large" sx={{ mx: 2 }} onMouseOver={handleMouseOver2}>
               학원비
             </Button>
+            <Menu anchorEl={anchorEl2} open={Boolean(anchorEl2)} onClose={handleClose2}>
+              <MenuItem onClick={handleClose2}>학원비 납부 목록</MenuItem>
+              <MenuItem onClick={handleClose2}>학원비 구성</MenuItem>
+              <MenuItem onClick={handleClose2}>학원비 청구</MenuItem>
+            </Menu>
             <Button color="inherit" size="large" sx={{ mx: 2 }}>
               전체 공지
             </Button>
