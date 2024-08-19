b418f82a7e98fe381ecad953207711f5cd6f0f82
haram8009 <haram8009@gmail.com>
2024-08-19 13:34:18 +0900

    feat: 강사페이지, 컴포넌트 생성

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as TeacherHeader } from './header/teacherHeader';
+
+export { default as TeacherSideBar } from './sidebar/teacherSidebar';
+
+export { default as Title } from './textLayout/title';
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export { default as NotFound } from './notFound/notFound';
 export { default as SignUp } from './signup/signup';
 export { default as Login } from './login/login';
+
+export { default as TeacherHome } from './home/teacherHome';
