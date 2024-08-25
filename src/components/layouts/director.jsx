008b2bf69bbd35efaf57c5c765e9be0a1c3b05e8
godeka <godeka@naver.com>
2024-08-25 15:34:49 +0900

    docs: JSDoc 주석 추가 #8

--- src/components/layouts/director.jsx
+++ src/components/layouts/director.jsx
@@ -4,6 +4,16 @@ import { Box } from '@mui/material';
 
 import DirectorHeader from '../header/directorHeader';
 
+/**
+ *@description 원장화면 헤더 있는 레이아웃
+
+ @example 
+ <Director>
+   Contents
+ </Director>
+ 
+ */
+
 export default function Director({ children }) {
   return (
     <Box component="main">
