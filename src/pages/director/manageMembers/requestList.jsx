2a59f9076802d6be77a38d175b8db6e08174b878
godeka <godeka@naver.com>
2024-08-29 14:35:39 +0900

    design: 등록요청목록 화면 박스 컴포넌트 제거 #11

--- src/pages/director/manageMembers/requestList.jsx
+++ src/pages/director/manageMembers/requestList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Box, Typography, List, ListItem, ListItemText, Button, Grid, Dialog, DialogTitle, DialogActions, DialogContent } from '@mui/material';
+import { Typography, List, ListItem, ListItemText, Button, Grid, Dialog, DialogActions, DialogContent } from '@mui/material';
 
 import Director from '../../../components/layouts/director';
 
@@ -23,135 +23,133 @@ export default function RequestList() {
 
   return (
     <Director>
-      <Box m={2}>
-        <Typography variant="h5" mb={3} p={1}>
-          등록 요청 목록
-        </Typography>
-        <Grid container spacing="10vw">
-          <Grid item xs={6}>
-            <Typography variant="h6" p={1.5}>
-              강사 등록 요청
-            </Typography>
-            <List sx={{ overflow: 'auto', maxHeight: '50vh', bgcolor: 'background.paper' }}>
-              <ListItem>
-                <ListItemText primary="나미리" secondary="과목: 미적분 1" />
-                <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
-                  승인
-                </Button>
-                <Button variant="contained" onClick={handleOpenDecline}>
-                  거절
-                </Button>
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="이하람" secondary="과목: 화법과 작문" />
-                <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
-                  승인
-                </Button>
-                <Button variant="contained" onClick={handleOpenDecline}>
-                  거절
-                </Button>
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="권해담" secondary="과목: 물리 1" />
-                <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
-                  승인
-                </Button>
-                <Button variant="contained" onClick={handleOpenDecline}>
-                  거절
-                </Button>
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="김대성" secondary="과목: 확률과 통계" />
-                <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
-                  승인
-                </Button>
-                <Button variant="contained" onClick={handleOpenDecline}>
-                  거절
-                </Button>
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="김대성" secondary="과목: 확률과 통계" />
-                <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
-                  승인
-                </Button>
-                <Button variant="contained" onClick={handleOpenDecline}>
-                  거절
-                </Button>
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="김대성" secondary="과목: 확률과 통계" />
-                <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
-                  승인
-                </Button>
-                <Button variant="contained" onClick={handleOpenDecline}>
-                  거절
-                </Button>
-              </ListItem>
-            </List>
-          </Grid>
-          <Grid item xs={6}>
-            <Typography variant="h6" p={1.5}>
-              학생 등록 요청
-            </Typography>
-            <List sx={{ overflow: 'auto', maxHeight: '50vh', bgcolor: 'background.paper' }}>
-              <ListItem>
-                <ListItemText primary="신짱구" secondary="학부모: 봉미선" />
-                <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
-                  승인
-                </Button>
-                <Button variant="contained" onClick={handleOpenDecline}>
-                  거절
-                </Button>
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="신짱아" secondary="학부모: 봉미선" />
-                <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
-                  승인
-                </Button>
-                <Button variant="contained" onClick={handleOpenDecline}>
-                  거절
-                </Button>
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="김철수" secondary="학부모: 김미영" />
-                <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
-                  승인
-                </Button>
-                <Button variant="contained" onClick={handleOpenDecline}>
-                  거절
-                </Button>
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="이훈이" secondary="학부모: 토마토" />
-                <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
-                  승인
-                </Button>
-                <Button variant="contained" onClick={handleOpenDecline}>
-                  거절
-                </Button>
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="이훈이" secondary="학부모: 토마토" />
-                <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
-                  승인
-                </Button>
-                <Button variant="contained" onClick={handleOpenDecline}>
-                  거절
-                </Button>
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="이훈이" secondary="학부모: 토마토" />
-                <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
-                  승인
-                </Button>
-                <Button variant="contained" onClick={handleOpenDecline}>
-                  거절
-                </Button>
-              </ListItem>
-            </List>
-          </Grid>
+      <Typography variant="h5" sx={{ mt: 2, mb: 5 }}>
+        등록 요청 목록
+      </Typography>
+      <Grid container spacing="10vw">
+        <Grid item xs={6}>
+          <Typography variant="h6" p={1.5}>
+            강사 등록 요청
+          </Typography>
+          <List sx={{ overflow: 'auto', maxHeight: '50vh', bgcolor: 'background.paper' }}>
+            <ListItem>
+              <ListItemText primary="나미리" secondary="과목: 미적분 1" />
+              <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
+                승인
+              </Button>
+              <Button variant="contained" onClick={handleOpenDecline}>
+                거절
+              </Button>
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="이하람" secondary="과목: 화법과 작문" />
+              <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
+                승인
+              </Button>
+              <Button variant="contained" onClick={handleOpenDecline}>
+                거절
+              </Button>
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="권해담" secondary="과목: 물리 1" />
+              <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
+                승인
+              </Button>
+              <Button variant="contained" onClick={handleOpenDecline}>
+                거절
+              </Button>
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="김대성" secondary="과목: 확률과 통계" />
+              <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
+                승인
+              </Button>
+              <Button variant="contained" onClick={handleOpenDecline}>
+                거절
+              </Button>
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="김대성" secondary="과목: 확률과 통계" />
+              <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
+                승인
+              </Button>
+              <Button variant="contained" onClick={handleOpenDecline}>
+                거절
+              </Button>
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="김대성" secondary="과목: 확률과 통계" />
+              <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
+                승인
+              </Button>
+              <Button variant="contained" onClick={handleOpenDecline}>
+                거절
+              </Button>
+            </ListItem>
+          </List>
         </Grid>
-      </Box>
+        <Grid item xs={6}>
+          <Typography variant="h6" p={1.5}>
+            학생 등록 요청
+          </Typography>
+          <List sx={{ overflow: 'auto', maxHeight: '50vh', bgcolor: 'background.paper' }}>
+            <ListItem>
+              <ListItemText primary="신짱구" secondary="학부모: 봉미선" />
+              <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
+                승인
+              </Button>
+              <Button variant="contained" onClick={handleOpenDecline}>
+                거절
+              </Button>
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="신짱아" secondary="학부모: 봉미선" />
+              <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
+                승인
+              </Button>
+              <Button variant="contained" onClick={handleOpenDecline}>
+                거절
+              </Button>
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="김철수" secondary="학부모: 김미영" />
+              <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
+                승인
+              </Button>
+              <Button variant="contained" onClick={handleOpenDecline}>
+                거절
+              </Button>
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="이훈이" secondary="학부모: 토마토" />
+              <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
+                승인
+              </Button>
+              <Button variant="contained" onClick={handleOpenDecline}>
+                거절
+              </Button>
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="이훈이" secondary="학부모: 토마토" />
+              <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
+                승인
+              </Button>
+              <Button variant="contained" onClick={handleOpenDecline}>
+                거절
+              </Button>
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="이훈이" secondary="학부모: 토마토" />
+              <Button variant="outlined" sx={{ mr: 1 }} onClick={handleOpenApprove}>
+                승인
+              </Button>
+              <Button variant="contained" onClick={handleOpenDecline}>
+                거절
+              </Button>
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
       <Dialog open={openApprove} onClose={handleCloseApprove}>
         <DialogContent>나미리님의 등록 요청을 승인하시겠습니까?</DialogContent>
         <DialogActions>
