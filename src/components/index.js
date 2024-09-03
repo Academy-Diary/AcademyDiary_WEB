aef638bc25efbaa93d981df536aa642062b8aab6
godeka <godeka@naver.com>
2024-09-03 16:56:29 +0900

    feat: 강의생성 - 수강생 등록 페이지 추가 #16

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,5 @@ export { default as TeacherSideBar } from './sidebar/teacherSidebar';
 export { default as Title } from './textLayout/title';
 
 export { default as Teacher } from './layouts/teacher';
+
+export { default as TransferList } from './transferList';
--- src/pages/director/manageCourses/addCourse.jsx
+++ src/pages/director/manageCourses/addCourse.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
 
-import { Typography, Box, Button, TextField, Select, MenuItem, InputLabel, FormControl, IconButton, Grid } from '@mui/material';
+import { Typography, Box, Button, TextField, Select, MenuItem, InputLabel, FormControl, IconButton, Grid, Dialog, DialogActions } from '@mui/material';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 import Director from '../../../components/layouts/director';
+import { TransferList } from '../../../components';
 
 const teachers = ['나미리', '이하람', '권해담', '김대성'];
+const students = ['신짱구', '신짱아', '이훈이', '김철수'];
 
 export default function AddCourse() {
   const [teacher, setTeacher] = useState('');
+  const [open, setOpen] = useState(false);
 
   const handleChangeTeacher = (e) => {
     setTeacher(e.target.value);
+  };
+  const handleOpenDialog = () => {
+    setOpen(true);
+  };
+  const handleCloseDialog = () => {
+    setOpen(false);
+  };
+  const handleClickRegister = () => {
+    setOpen(false);
   };
 
   return (
@@ -44,7 +56,7 @@ export default function AddCourse() {
                 <Typography sx={{ py: 1 }}>수강생 등록</Typography>
               </Grid>
               <Grid item xs={11}>
-                <IconButton>
+                <IconButton onClick={handleOpenDialog}>
                   <OpenInNewIcon />
                 </IconButton>
               </Grid>
@@ -59,6 +71,23 @@ export default function AddCourse() {
             </Button>
           </Box>
         </Grid>
+        <Dialog open={open} onClose={handleCloseDialog}>
+          <Grid container spacing={5} sx={{ py: 3, px: 5 }}>
+            <Grid item xs={12}>
+              <Typography variant="h6">수강생 등록</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TransferList leftList={students} rightList={[]} />
+            </Grid>
+            <Grid item xs={12}>
+              <DialogActions>
+                <Button variant="contained" onClick={handleClickRegister}>
+                  등록 완료
+                </Button>
+              </DialogActions>
+            </Grid>
+          </Grid>
+        </Dialog>
       </Box>
     </Director>
   );
