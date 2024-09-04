970997d0abffd34975d82b6d078547887a8fc6b4
godeka <godeka@naver.com>
2024-09-04 19:41:59 +0900

    feat: 폐강 다이얼로그 추가 #16

--- src/pages/director/manageCourses/manageCourses.jsx
+++ src/pages/director/manageCourses/manageCourses.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { List, ListItem, ListItemText, Typography, Grid, Box, Button, ButtonGroup } from '@mui/material';
+import { List, ListItem, ListItemText, Typography, Grid, Box, Button, ButtonGroup, Dialog, DialogContent, DialogContentText, DialogActions, DialogTitle } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 
 import Director from '../../../components/layouts/director';
@@ -15,12 +15,21 @@ const courses = [
 
 export default function ManageCourses() {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState(null);
 
   const handleClickAdd = () => {
     navigate('/director/manage-courses/add-course');
   };
   const handleClickDetails = () => {
     navigate('/director/manage-courses/course-details');
+  };
+
+  const handleOpenDialog = () => {
+    setOpen(true);
+  };
+  const handleCloseDialog = () => {
+    setOpen(false);
   };
 
   return (
@@ -30,7 +39,26 @@ export default function ManageCourses() {
       </Typography>
       <List sx={{ maxHeight: '70vh', overflow: 'auto' }}>
         {courses.map((course) => (
-          <CourseItem key={course.name} name={course.name} teacher={course.teacher} numStudents={course.numStudents} handleClickDetails={handleClickDetails} />
+          <ListItem key={course.name} sx={{ height: 120, marginY: 2, backgroundColor: 'lightgray' }}>
+            <ListItemText primary={course.name} secondary={course.teacher} />
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <ListItemText align="right" secondary={`수강 인원: ${course.numStudents}`} sx={{ mb: 2 }} />
+              <ButtonGroup size="small">
+                <Button variant="outlined" onClick={handleClickDetails}>
+                  강의 상세
+                </Button>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    handleOpenDialog();
+                    setSelected(course);
+                  }}
+                >
+                  폐강하기
+                </Button>
+              </ButtonGroup>
+            </Box>
+          </ListItem>
         ))}
       </List>
       <Grid container justifyContent="flex-end" sx={{ position: 'fixed', bottom: '3vh', right: '3vw' }}>
@@ -40,23 +68,22 @@ export default function ManageCourses() {
           </Button>
         </Grid>
       </Grid>
+      {selected && (
+        <Dialog open={open} onClose={handleCloseDialog}>
+          <DialogTitle>{selected.name} 강의를 폐강하시겠습니까?</DialogTitle>
+          <DialogContent>
+            <Box sx={{ padding: 2, backgroundColor: 'lightgrey' }}>
+              <DialogContentText>강의명: {selected.name}</DialogContentText>
+              <DialogContentText>강사명: {selected.teacher}</DialogContentText>
+              <DialogContentText>수강인원: {selected.numStudents}</DialogContentText>
+            </Box>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseDialog}>취소</Button>
+            <Button onClick={handleCloseDialog}>폐강</Button>
+          </DialogActions>
+        </Dialog>
+      )}
     </Director>
-  );
-}
-
-function CourseItem({ name, teacher, numStudents, handleClickDetails }) {
-  return (
-    <ListItem sx={{ height: 120, marginY: 2, backgroundColor: 'lightgray' }}>
-      <ListItemText primary={name} secondary={teacher} />
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <ListItemText align="right" secondary={`수강 인원: ${numStudents}`} sx={{ mb: 2 }} />
-        <ButtonGroup size="small">
-          <Button variant="outlined" onClick={handleClickDetails}>
-            강의 상세
-          </Button>
-          <Button variant="contained">폐강하기</Button>
-        </ButtonGroup>
-      </Box>
-    </ListItem>
   );
 }
