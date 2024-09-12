d9efd8441a21a2b97c733483bcad8f6f5c7d5e12
godeka <godeka@naver.com>
2024-09-12 14:52:03 +0900

    feat: 수강반 추가 다이얼로그 추가 #22

--- src/pages/director/tuitionFees/makeClass.jsx
+++ src/pages/director/tuitionFees/makeClass.jsx
@@ -1,5 +1,7 @@
-import { Typography, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Grid, Button } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Typography, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Grid, Button, Box, Dialog, DialogTitle, DialogContent, TextField, DialogActions } from '@mui/material';
+import { Add } from '@mui/icons-material';
 
 const classes = [
   { name: '수학 집중반', fee: 270000, due: '2024.01.01~2024.01.31' },
@@ -8,6 +10,15 @@ const classes = [
 ];
 
 export default function MakeClass() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpenDialog = () => {
+    setOpen(true);
+  };
+  const handleCloseDialog = () => {
+    setOpen(false);
+  };
+
   return (
     <>
       <Typography variant="h6" sx={{ pt: 2, pb: 3 }}>
@@ -45,6 +56,27 @@ export default function MakeClass() {
           </TableBody>
         </Table>
       </TableContainer>
+      <Box sx={{ position: 'fixed', right: '3vw', bottom: '3vh' }}>
+        <Button variant="contained" size="large" startIcon={<Add />} onClick={handleOpenDialog}>
+          수강반 추가
+        </Button>
+      </Box>
+      <Dialog component="form" open={open} onClose={handleCloseDialog}>
+        <DialogTitle>수강반 추가</DialogTitle>
+        <DialogContent sx={{ mt: 3 }}>
+          <TextField label="수강반 이름" required fullWidth sx={{ mb: 2 }} />
+          <TextField label="기간 (일)" type="number" required fullWidth sx={{ mb: 2 }} />
+          <TextField label="가격" type="number" required fullWidth sx={{ mb: 2 }} />
+        </DialogContent>
+        <DialogActions sx={{ m: 3 }}>
+          <Button variant="outlined" onClick={handleCloseDialog}>
+            취소
+          </Button>
+          <Button type="submit" variant="contained">
+            완료
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 }
