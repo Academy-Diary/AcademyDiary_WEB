270ea0b651c930d800a986df886f9830f74dfe52
godeka <godeka@naver.com>
2024-10-07 20:27:38 +0900

    feat: 아이디 찾기 실패 처리 #42

--- src/pages/login/findId.jsx
+++ src/pages/login/findId.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 
-import { Box, Button, TextField, Grid, Typography } from '@mui/material';
+import { Box, Button, TextField, Grid, Typography, Alert } from '@mui/material';
 import { TitleMedium } from '../../components';
 import useFindId from '../../api/queries/user/useFindId';
 
 export default function FindId() {
   const [found, setFound] = useState(false);
   const [userId, setUserId] = useState('');
+  const [isError, setIsError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState('');
 
   const findIdMutation = useFindId();
 
@@ -23,10 +25,12 @@ export default function FindId() {
       onSuccess: (res) => {
         setFound(true);
         setUserId(res.data.user_id);
-        console.log(res.data.user_id);
       },
       onError: (error) => {
-        console.log(error.message);
+        setIsError(true);
+        if (error.errorCode === 400) setErrorMsg('이메일 혹은 전화번호 형식을 올바르게 입력해주세요.');
+        else if (error.errorCode === 404) setErrorMsg('계정이 존재하지 않습니다.');
+        else if (error.errorCode === 500) setErrorMsg('서버 오류로 아이디를 찾을 수 없습니다.');
       },
     });
   };
@@ -49,10 +53,13 @@ export default function FindId() {
         <Box component="form" sx={{ mt: 5 }} onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <TextField label="이메일" name="email" required fullWidth />
+              <TextField label="이메일" name="email" required fullWidth error={isError} />
             </Grid>
             <Grid item xs={12}>
-              <TextField label="전화번호" name="phonenumber" required fullWidth />
+              <TextField label="전화번호" name="phonenumber" required fullWidth error={isError} />
+            </Grid>
+            <Grid item xs={12}>
+              {isError && <Alert severity="error">{errorMsg}</Alert>}
             </Grid>
             <Grid item xs={12}>
               <Button type="submit" variant="contained" size="large" fullWidth sx={{ mt: 3 }}>
