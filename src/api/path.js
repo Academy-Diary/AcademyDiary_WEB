9eb5da7ffd916a63edd6c4505cb04b0fea2bcabb
godeka <godeka@naver.com>
2024-10-07 18:04:10 +0900

    feat: 아이디 찾기 api 연동 #42

--- src/api/path.js
+++ src/api/path.js
@@ -4,6 +4,7 @@ export const PATH_API = {
   API_DOMAIN: SECRET.server_ip,
   // user
   SIGN_IN: '/user/login',
+  FIND_ID: '/user/find-id',
   SIGN_UP: '/user/signup',
   CHECK_DUP: '/user/check-id',
   SIGN_OUT: '/user/logout',
--- src/pages/login/findId.jsx
+++ src/pages/login/findId.jsx
@@ -2,21 +2,43 @@ import React from 'react';
 
 import { Box, Button, TextField, Grid } from '@mui/material';
 import { TitleMedium } from '../../components';
+import useFindId from '../../api/queries/user/useFindId';
 
 export default function FindId() {
+  const findIdMutation = useFindId();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const submitData = {
+      email: data.get('email'),
+      phone_number: data.get('phonenumber'),
+    };
+
+    // console.log(submitData);
+    findIdMutation.mutate(submitData, {
+      onSuccess: (res) => {
+        console.log(res.data.user_id);
+      },
+      onError: (error) => {
+        console.log(error.message);
+      },
+    });
+  };
+
   return (
     <>
       <TitleMedium title="아이디 찾기" />
-      <Box component="form" sx={{ mt: 5 }}>
+      <Box component="form" sx={{ mt: 5 }} onSubmit={handleSubmit}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
-            <TextField label="이메일" required fullWidth />
+            <TextField label="이메일" name="email" required fullWidth />
           </Grid>
           <Grid item xs={12}>
-            <TextField label="전화번호" required fullWidth />
+            <TextField label="전화번호" name="phonenumber" required fullWidth />
           </Grid>
           <Grid item xs={12}>
-            <Button variant="contained" fullWidth sx={{ mt: 3 }}>
+            <Button type="submit" variant="contained" size="large" fullWidth sx={{ mt: 3 }}>
               찾기
             </Button>
           </Grid>
