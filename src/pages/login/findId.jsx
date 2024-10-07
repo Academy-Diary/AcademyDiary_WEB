63371bc0e909807db82f5baa17ae3aaa5adcbd6e
godeka <godeka@naver.com>
2024-10-07 16:59:03 +0900

    design: 아이디 찾기 화면 기본 디자인 #42

--- src/pages/login/findId.jsx
+++ src/pages/login/findId.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, TextField, Grid } from '@mui/material';
 import { TitleMedium } from '../../components';
 
 export default function FindId() {
   return (
     <>
       <TitleMedium title="아이디 찾기" />
-      <Box component="form">
-        <TextField label="이메일" />
-        <TextField label="전화번호" />
-        <Button variant="contained">찾기</Button>
+      <Box component="form" sx={{ mt: 5 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField label="이메일" required fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label="전화번호" required fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" fullWidth sx={{ mt: 3 }}>
+              찾기
+            </Button>
+          </Grid>
+        </Grid>
       </Box>
     </>
   );
