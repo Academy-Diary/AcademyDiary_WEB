517672186565dcdea0b76b536f26d1d15eaccf70
Minsoo Kim <ms214@ms214.kr>
2024-10-08 15:41:26 +0900

    #31 feat: 과목별 공지목록 페이지 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ export { default as Login } from './login/login';
 export { default as TeacherHome } from './home/teacherHome';
 export { default as ClassHome } from './teacher/class/classHome';
 export { default as ClassPage } from './teacher/class/classPage';
+export { default as CourseNotice } from './teacher/notice/courseNotice';
 export { default as TeacherNotice } from './teacher/notice/teacherNotice';
 export { default as TeacherNoticeDetails } from './teacher/notice/noticeDetails';
 export { default as Register } from './register/register';
