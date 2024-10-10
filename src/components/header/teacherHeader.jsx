1834738d7e25d064a336cd4dbf5b2d7caf2a8423
Minsoo Kim <ms214@ms214.kr>
2024-10-10 23:10:03 +0900

    feat: 프로필사진 클릭시 서브메뉴 띄우기

--- src/components/header/teacherHeader.jsx
+++ src/components/header/teacherHeader.jsx
@@ -1,11 +1,21 @@
 import React, { useState } from 'react';
-import { Avatar, Box, Typography } from '@mui/material';
+import { Avatar, Box, Divider, ListItemIcon, ListItemText, Menu, MenuItem, Typography } from '@mui/material';
+import { AccountBoxRounded, LogoutRounded } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
 import { useUserAuthStore } from '../../store';
 
 export default function TeacherHeader() {
   const { user } = useUserAuthStore();
   const navigate = useNavigate();
+
+  const [isMouseOver, setMouseOver] = useState(null);
+
+  const handleClick = (e) => {
+    setMouseOver(e.currentTarget);
+  };
+  const handleClose = () => {
+    setMouseOver(null);
+  };
 
   return (
     <Box>
@@ -21,7 +31,32 @@ export default function TeacherHeader() {
         </Typography>
         <Box display="flex" alignItems="center" gap={1}>
           <Typography>강사: {user?.user_name}</Typography>
-          <Avatar />
+          <Avatar onClick={handleClick} />
+          <Menu anchorEl={isMouseOver} open={Boolean(isMouseOver)} onClose={handleClose}>
+            <MenuItem
+              onClick={() => {
+                handleClose();
+                navigate('/teacher/profile');
+              }}
+            >
+              <ListItemIcon>
+                <AccountBoxRounded fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>profile</ListItemText>
+            </MenuItem>
+            <Divider />
+            <MenuItem
+              onClick={() => {
+                handleClose();
+                console.log('Log out');
+              }}
+            >
+              <ListItemIcon>
+                <LogoutRounded fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Logout</ListItemText>
+            </MenuItem>
+          </Menu>
         </Box>
       </Box>
     </Box>
