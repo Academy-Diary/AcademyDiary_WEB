9a83967b166611258b6bbcb9b0f190fee2f7a6fd
godeka <godeka@naver.com>
2024-10-14 23:39:44 +0900

    fix: 불필요한 api 호출 대신 userAuthStore에서 프로필정보 띄우기 #35

--- src/pages/director/profile/directorProfile.jsx
+++ src/pages/director/profile/directorProfile.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 
 import { Avatar, Box, Container, Grid, IconButton, Typography } from '@mui/material';
 import { Edit } from '@mui/icons-material';
-import { useProfileBasic } from '../../../api/queries/user/useProfile';
 import { useUserAuthStore } from '../../../store';
 
 // 회원 basic info
@@ -23,8 +22,6 @@ export default function DirectorProfile() {
   const navigate = useNavigate();
   const { user } = useUserAuthStore();
 
-  const { data: basicInfo } = useProfileBasic(user.user_id);
-
   const handleClickUpdate = () => {
     navigate('/director/profile/update');
   };
@@ -36,7 +33,7 @@ export default function DirectorProfile() {
           <Avatar sx={{ width: 100, height: 100 }} />
         </Grid>
         <Grid item xs={8} sx={{ display: 'flex', alignItems: 'center' }}>
-          <Typography variant="h6">{basicInfo?.user_name} 원장</Typography>
+          <Typography variant="h6">{user.user_name} 원장</Typography>
           <IconButton sx={{ m: 2 }} onClick={handleClickUpdate}>
             <Edit />
           </IconButton>
@@ -44,9 +41,9 @@ export default function DirectorProfile() {
         <Grid item xs={12}>
           <Typography variant="h6">개인 정보</Typography>
           <Box sx={{ p: 2, backgroundColor: 'lightgray' }}>
-            <Typography variant="body1">생년월일: {basicInfo?.birth_date}</Typography>
-            <Typography variant="body1">전화번호: {basicInfo?.phone_number}</Typography>
-            <Typography variant="body1">이메일: {basicInfo?.email}</Typography>
+            <Typography variant="body1">생년월일: {user.birth_date}</Typography>
+            <Typography variant="body1">전화번호: {user.phone_number}</Typography>
+            <Typography variant="body1">이메일: {user.email}</Typography>
           </Box>
         </Grid>
         <Grid item xs={12}>
