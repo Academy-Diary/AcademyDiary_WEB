d035fa908c9224537e06b1c5b29b8a274cee5a61
godeka <godeka@naver.com>
2024-10-22 21:28:39 +0900

    fix: api 응답 인터셉터에도 토큰 재발급 요청 추가 #45

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 import { PATH_API } from './path';
+import { PATH } from '../route/path';
 
 const TIMEOUT_TIME = 10_000;
 
@@ -45,24 +46,36 @@ axiosInstance.interceptors.request.use(
 axiosInstance.interceptors.response.use(
   (response) => response,
   async (error) => {
-    // invalid token
-    // const originalRequest = error.config;
-    // /* eslint-disable no-underscore-dangle */
-    // if (error.response.status === 401 && !originalRequest._retry) {
-    //   originalRequest._retry = true;
+    // 액세스 토큰이 만료된 경우
+    const originalRequest = error.config;
 
-    //   // 리프레시 토큰도 만료된 경우 로그아웃 처리
-    //   alert('system.axios-401-error');
-    //   localStorage.removeItem('accessToken');
-    //   delete originalRequest.Authorization;
+    if (error.response.status === 403) {
+      // originalRequest._retry = true; // 무한 재요청 방지
 
-    //   // window.location.reload();
-    //   window.location.href = PATH.root;
-    //   // Promise.resolve('Error! failed token refresh');
-    //   // }
-    //   // }
-    // }
-    // /* eslint-enable no-underscore-dangle */
+      try {
+        const res = await axiosInstance.post(PATH_API.REISSUE_TOKEN);
+        const newAccessToken = res.data.accessToken;
+        // console.log(newAccessToken);
+        // console.log(isValidToken(newAccessToken));
+
+        localStorage.setItem('accessToken', newAccessToken);
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        // api 재요청
+        return axios(originalRequest);
+      } catch {
+        // TODO: 잘 되는지 확인 필요
+        // 리프레시 토큰도 만료된 경우 로그아웃 처리
+        localStorage.removeItem('accessToken');
+        delete originalRequest.headers.Authorization;
+
+        // window.location.reload();
+        window.location.href = PATH.root;
+        // Promise.resolve('Error! failed token refresh');
+        // }
+        // }
+      }
+    }
 
     // timeout
     if (axios.isCancel(error)) {
