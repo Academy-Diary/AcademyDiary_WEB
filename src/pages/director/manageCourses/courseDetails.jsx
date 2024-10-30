1343227ef573d82794c4af5633060ad02a6be238
godeka <godeka@naver.com>
2024-10-30 22:07:50 +0900

    fix: 강의 상세에 강의 요일,시간 추가 #32

--- src/pages/director/manageCourses/courseDetails.jsx
+++ src/pages/director/manageCourses/courseDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Typography, Box, TextField, Grid, Paper, Table, TableHead, TableBody, TableRow, TableCell, TableContainer } from '@mui/material';
@@ -6,11 +6,25 @@ import { TitleMedium, BottomTwoButtons } from '../../../components';
 import { useLectureStore } from '../../../store';
 import { useAttendeeList } from '../../../api/queries/lectures/useAttendeeList';
 
+const weekdaysKor = {
+  MONDAY: '월',
+  TUESDAY: '화',
+  WEDNESDAY: '수',
+  THURSDAY: '목',
+  FRIDAY: '금',
+};
+
 export default function CourseDetails() {
   const navigate = useNavigate();
 
   const { lecture } = useLectureStore();
   const { data: attendees } = useAttendeeList(lecture.lecture_id);
+
+  const [lectureDays, setLectureDays] = useState(''); // ex) "월, 화, 수"
+  useEffect(() => {
+    const days = lecture.days.map((lec) => weekdaysKor[lec]);
+    setLectureDays(days.join(', '));
+  }, [lecture]);
 
   const handleCancle = () => {
     navigate('/director/manage-courses/');
@@ -31,6 +45,12 @@ export default function CourseDetails() {
           <Grid item xs={12}>
             <Typography>강사명</Typography>
             <TextField label="강사명" value={lecture.teacher_name} sx={{ mt: 2 }} disabled />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography>강의 시간</Typography>
+            <TextField label="요일" value={lectureDays} sx={{ mt: 2 }} disabled />
+            <TextField label="시작 시간" value={lecture.start_time} sx={{ ml: 4, mt: 2, minWidth: 150 }} disabled />
+            <TextField label="종료 시간" value={lecture.end_time} sx={{ ml: 2, mt: 2, minWidth: 150 }} disabled />
           </Grid>
           <Grid item xs={12}>
             <Typography sx={{ py: 1 }}>수강생 목록</Typography>
