17b1ba0b62a01a10d671d09b000cbf644133bee5
godeka <godeka@naver.com>
2024-11-05 15:18:50 +0900

    feat: 학생관리 학생 또는 학부모 이름으로 검색 기능 추가 #28

--- src/pages/director/manageMembers/manageStudents.jsx
+++ src/pages/director/manageMembers/manageStudents.jsx
@@ -1,6 +1,25 @@
 import React, { useState } from 'react';
 
-import { Typography, TableContainer, Paper, Table, TableHead, TableBody, TableRow, TableCell, Button, Dialog, DialogContent, DialogActions, DialogContentText, DialogTitle, Box } from '@mui/material';
+import {
+  Typography,
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Button,
+  Dialog,
+  DialogContent,
+  DialogActions,
+  DialogContentText,
+  DialogTitle,
+  Box,
+  OutlinedInput,
+} from '@mui/material';
+import { Search } from '@mui/icons-material';
+
 import { TitleMedium } from '../../../components';
 import { useStudentList } from '../../../api/queries/members/useStudentList';
 import { useUserAuthStore } from '../../../store';
@@ -27,6 +46,7 @@ import { useDeleteStudent } from '../../../api/queries/members/useDeleteStudent'
 export default function ManageStudents() {
   const [open, setOpen] = useState(false);
   const [selected, setSelected] = useState({ name: '', parentName: '', phone: '', parentPhone: '' });
+  const [searchInput, setSearchInput] = useState('');
 
   const { user } = useUserAuthStore();
   const { data: students } = useStudentList(user.academy_id);
@@ -34,6 +54,11 @@ export default function ManageStudents() {
 
   const handleCloseDialog = () => {
     setOpen(false);
+  };
+
+  const handleInputSearch = (e) => {
+    const { value } = e.target;
+    setSearchInput(value);
   };
 
   const handleClickDelete = (selectedStudent) => {
@@ -50,6 +75,7 @@ export default function ManageStudents() {
     <>
       <TitleMedium title="학생 관리" />
       <Typography mb={2}>학생 인원: {students?.length}</Typography>
+      <OutlinedInput placeholder="학생 또는 학부모 이름" endAdornment={<Search />} onChange={handleInputSearch} sx={{ mb: 2 }} />
       <TableContainer component={Paper} sx={{ maxHeight: '65vh', width: '80vw' }}>
         <Table stickyHeader sx={{ minWidth: 650 }}>
           <TableHead>
@@ -65,7 +91,7 @@ export default function ManageStudents() {
             {students?.map((student) => {
               const parent = student.familiesAsStudent ? student.familiesAsStudent[0]?.parent : null;
 
-              return (
+              return student.user_name.includes(searchInput) || parent?.user_name.includes(searchInput) ? (
                 <TableRow key={student.user_id}>
                   <TableCell component="th" scope="row">
                     {student.user_name}
@@ -79,7 +105,7 @@ export default function ManageStudents() {
                     </Button>
                   </TableCell>
                 </TableRow>
-              );
+              ) : null;
             })}
           </TableBody>
         </Table>
