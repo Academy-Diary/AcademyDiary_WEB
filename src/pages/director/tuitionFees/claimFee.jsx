732f32b64c6525ba5f4ca731ff895ed269b26322
godeka <godeka@naver.com>
2024-11-06 15:42:17 +0900

    feat: 청구서 미리보기 추가 #33

--- src/pages/director/tuitionFees/claimFee.jsx
+++ src/pages/director/tuitionFees/claimFee.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Typography, Box, Grid, Select, FormControl, InputLabel, MenuItem, ToggleButton, ToggleButtonGroup, Button } from '@mui/material';
+import { Typography, Box, Grid, Select, FormControl, InputLabel, MenuItem, ToggleButton, ToggleButtonGroup, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -34,10 +34,14 @@ import { useClassList } from '../../../api/queries/tuitionFees/useClassList';
 //   },
 // ];
 
+// 총 청구 비용 계산
+const getTotalFee = (fees) => fees.reduce((acc, cur) => acc + cur, 0);
+
 export default function ClaimFee() {
   const [selectedStudent, setSelectedStudent] = useState('');
   const [selectedClasses, setSelectedClasses] = useState([]);
   const [dueDate, setDueDate] = useState(null);
+  const [open, setOpen] = useState(false);
 
   const { user } = useUserAuthStore();
   const { data: students } = useStudentList(user.academy_id);
@@ -50,10 +54,15 @@ export default function ClaimFee() {
     setSelectedClasses(newList);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setOpen(true);
+  };
+
   return (
     <>
       <TitleMedium title="학원비 청구" />
-      <Box component="form" sx={{ mt: 5 }}>
+      <Box component="form" sx={{ mt: 5 }} onSubmit={handleSubmit}>
         <Grid container spacing={6}>
           <Grid item xs={12}>
             <Typography>학생 선택</Typography>
@@ -86,16 +95,56 @@ export default function ClaimFee() {
           </Grid>
         </Grid>
         <Box sx={{ position: 'fixed', left: '3vw', bottom: '5vh' }}>
-          <Typography variant="h6">{`총 청구 비용: ${selectedClasses.map((c) => c.expense).reduce((acc, cur) => acc + cur, 0)} ₩`}</Typography>
+          <Typography variant="h6">{`총 청구 비용: ${getTotalFee(selectedClasses.map((c) => c.expense))} ₩`}</Typography>
         </Box>
         <Box sx={{ position: 'fixed', bottom: '3vh', right: '3vw' }}>
-          <Button size="large" variant="outlined" sx={{ mr: 2 }}>
-            미리보기
-          </Button>
           <Button type="submit" size="large" variant="contained">
-            청구서 전송하기
+            청구하기
           </Button>
         </Box>
+        <Dialog open={open} onClose={() => setOpen(false)} maxWidth="sm" fullWidth>
+          <DialogTitle>청구서 미리보기</DialogTitle>
+          <DialogContent>
+            <Grid container spacing={2}>
+              <Grid item xs={2}>
+                <Typography>청구 대상</Typography>
+              </Grid>
+              <Grid item xs={10}>
+                <Typography>{`${selectedStudent?.user_name}(${selectedStudent?.phone_number?.substr(9)})`}</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Box sx={{ border: '1px solid black', padding: 2 }}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={3}>
+                      <Typography>수강반</Typography>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <Typography>{selectedClasses.map((c, idx) => (idx < selectedClasses.length - 1 ? `${c.class_name}(${c.expense}), ` : `${c.class_name}(${c.expense})`))}</Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <Typography>총 청구 비용</Typography>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <Typography>{getTotalFee(selectedClasses.map((c) => c.expense))}₩</Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <Typography>납부 기한</Typography>
+                    </Grid>
+                    <Grid item xs={9}>
+                      <Typography>{dueDate?.format('~YYYY.MM.DD').toString()}</Typography>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Grid>
+            </Grid>
+          </DialogContent>
+          <DialogActions>
+            <Button variant="outlined" onClick={() => setOpen(false)}>
+              취소
+            </Button>
+            <Button variant="contained">전송하기</Button>
+          </DialogActions>
+        </Dialog>
       </Box>
     </>
   );
