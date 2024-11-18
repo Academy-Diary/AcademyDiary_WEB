6bfa0693fd6167240a88b9b45dd20eceb5dda886
godeka <godeka@naver.com>
2024-11-18 20:52:34 +0900

    feat: 원장 전체공지 목록 조회 api 연동 #34

--- src/pages/director/notice/directorNotice.jsx
+++ src/pages/director/notice/directorNotice.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import { Pagination } from '@mui/material';
 
 import { TitleMedium, Notice, AddButton } from '../../../components';
 import { useUserAuthStore } from '../../../store';
+import { useNoticeList } from '../../../api/queries/notice/useNoticeList';
 
-const notices = [
-  { id: 1, title: '8월 정기고사 안내', date: '2024-07-20', view: 55 },
-  { id: 2, title: '7월 정기고사 안내', date: '2024-06-18', view: 101 },
-  { id: 3, title: '6월 정기고사 안내', date: '2024-05-21', view: 129 },
-  { id: 4, title: '5월 정기고사 안내', date: '2024-04-21', view: 129 },
-  { id: 5, title: '4월 정기고사 안내', date: '2024-03-21', view: 129 },
-  { id: 6, title: '3월 정기고사 안내', date: '2024-02-29', view: 201 },
-];
+// const notices = {
+//   notice_count: 25,
+//   notice_list: [
+//     {
+//       title: '코로나19로 인한 학원 운영 방침',
+//       content: '코로나19 예방 방침 공지합니다.',
+//       user_id: 'test_chief',
+//       views: 123,
+//       notice_id: 'test_academy&0&5',
+//       created_at: '2024-11-16',
+//       updated_at: '2024-11-17',
+//     },
+//   ],
+// };
+
+const PAGE_SIZE = 6;
 
 export default function DirectorNotice() {
   const { user } = useUserAuthStore();
   const navigate = useNavigate();
+  const [page, setPage] = useState(1);
+
+  const { data: notices, refetch } = useNoticeList(0, page, PAGE_SIZE);
+
+  const handleChangePage = (e, value) => {
+    setPage(value);
+    refetch();
+  };
 
   const handleClickAdd = () => {
     navigate('/director/notice/add');
@@ -29,8 +48,8 @@ export default function DirectorNotice() {
   return (
     <>
       <TitleMedium title="전체 공지사항" />
-      {/* TODO: editable 주석 제거 */}
-      <Notice notices={notices} /* editable={user.role === 'CHIEF'} */ updateURL="/director/notice/update" handleClickDelete={handleClickDelete} />
+      <Notice notices={notices ? notices.notice_list : []} editable={user.role === 'CHIEF'} updateURL="/director/notice/update" handleClickDelete={handleClickDelete} />
+      <Pagination count={notices ? Math.trunc(notices.notice_count / PAGE_SIZE) + 1 : 1} page={page} onChange={handleChangePage} sx={{ position: 'fixed', bottom: 30 }} />
       <AddButton title="새 공지사항 등록" onClick={handleClickAdd} />
     </>
   );
