df7a9ac27dc83ba42ae6844088853e34c002b566
godeka <godeka@naver.com>
2024-11-28 17:17:20 +0900

    chore: ReactQueryProvider에 에러핸들링을 위한 queyCache 추가, defaultOptions은 retry 제외 모두 기본값으로 #94

--- src/api/react-query-provider.jsx
+++ src/api/react-query-provider.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider, QueryClient, QueryCache } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function ReactQueryProviders({ children }) {
   const [client] = useState(
     new QueryClient({
+      queryCache: new QueryCache({
+        onError: (error) => {
+          console.log('Error occured at useQuery:', error);
+        },
+      }),
       defaultOptions: {
         queries: {
-          refetchOnWindowFocus: false, // 윈도우가 다시 포커스되었을때 데이터를 refetch
-          refetchOnMount: false, // 데이터가 stale 상태이면 컴포넌트가 마운트될 때 refetch
-          retry: 1, // API 요청 실패시 재시도 하는 옵션 (설정값 만큼 재시도)
+          refetchOnWindowFocus: true, // 윈도우가 다시 포커스되었을때
+          refetchOnMount: true, // 새로운 컴포넌트 마운트가 발생한 경우
+          refetchOnReconnect: true, // 네트워크 재연결이 발생한 경우
+          staleTime: 0, // 데이터가 fresh -> stale 되는 시간
+          gcTime: 0, // 캐시된 데이터가 얼마나 오랫동안 메모리에 유지될 것인지
+          retry: 0, // API 요청 실패시 재시도 하는 횟수
         },
       },
     })
