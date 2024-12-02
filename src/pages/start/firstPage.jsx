b6455630732c1d4db33a78f78eac048e40a8cf75
godeka <godeka@naver.com>
2024-12-02 20:29:57 +0900

    design: 첫 화면 디자인 #108

--- src/pages/start/firstPage.jsx
+++ src/pages/start/firstPage.jsx
@@ -1,15 +1,22 @@
-import { Typography, Button } from '@mui/material';
+import React from 'react';
+import { Typography, Button, Box } from '@mui/material';
+
+import logo from '../../assets/logo.png';
 
 export default function FirstPage() {
   return (
     <>
-      <Typography variant="h4" align="center" sx={{ mt: 5, mb: 30 }}>
-        Academy Pro
+      <Box component="img" src={logo} sx={{ width: 200 }} />
+      <Typography variant="h4" align="center" sx={{ mb: 3 }}>
+        아카데미 다이어리
+      </Typography>
+      <Typography align="center" sx={{ mb: 25 }}>
+        우리들의 스마트한 학습 관리
       </Typography>
       <Button variant="contained" size="large" sx={{ m: 1 }} fullWidth href="/login">
         로그인
       </Button>
-      <Button variant="contained" size="large" sx={{ m: 1 }} fullWidth href="/signup">
+      <Button variant="contained" size="large" sx={{ m: 1 }} fullWidth href="/signup" color="secondary">
         회원가입
       </Button>
     </>
