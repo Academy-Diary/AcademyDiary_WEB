e2af581db67ea427735d2ec53b79575221be4d03
godeka <godeka@naver.com>
2024-12-02 20:28:18 +0900

    design: theme에 palette primary, secondary 색상 설정 #108

--- src/styles/globalTheme.js
+++ src/styles/globalTheme.js
@@ -1,5 +1,29 @@
 import { createTheme } from '@mui/material';
 
-const theme = createTheme();
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#024F51',
+    },
+    secondary: {
+      main: '#FEC749',
+    },
+  },
+  //   typography: {
+  //     fontFamily: 'Pretendard',
+  //   },
+  //   components: {
+  //     MuiCssBaseline: {
+  //       styleOverrides: `
+  //         @font-face {
+  //             font-family: 'Pretendard-Regular';
+  //             src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+  //             font-weight: 400;
+  //             font-style: normal;
+  //         }
+  //       `,
+  //     },
+  //   },
+});
 
 export default theme;
