18d7f24fd34817542fbac9ab636e291807f29568
Minsoo Kim <ms214@ms214.kr>
2024-12-02 22:23:56 +0900

    feat: 사이드바 클릭시 표시 추가 #110

--- src/components/layouts/sidebar/teacherSidebar.jsx
+++ src/components/layouts/sidebar/teacherSidebar.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, List, ListItemButton, ListItemText } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import clip from '../../../img/Binder clip.png';
 import lectureIcon from '../../../img/lecture_icon.png';
 import consultationIcon from '../../../img/consultation_icon.png';
@@ -8,12 +8,20 @@ import noticeIcon from '../../../img/notice_icon.png';
 
 export default function TeacherSidebar() {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
   const items = [
     { name: 'My 강의 목록', link: '/teacher/class', bgcolor: 'rgb(255, 211, 23, 0.6)', icon: lectureIcon },
     { name: '학생 상담', link: '/teacher/counseling', bgcolor: 'rgb(6, 68, 32, 0.6)', icon: consultationIcon },
     { name: '학원 공지', link: '/teacher/notice', bgcolor: 'rgb(2, 79, 81, 0.6)', icon: noticeIcon },
   ];
-  const [isClicked, setClicked] = useState();
+  const origin = { 'My 강의 목록': false, '학생 상담': false, '학원 공지': false };
+  const [isClicked, setClicked] = useState({ 'My 강의 목록': false, '학생 상담': false, '학원 공지': false });
+  const handleClickMenu = (name) => {
+    setClicked({ ...origin, [`${name}`]: true });
+  };
+  useEffect(() => {
+    if (pathname === '/teacher') setClicked({ 'My 강의 목록': false, '학생 상담': false, '학원 공지': false });
+  }, [pathname]);
   return (
     <Box sx={{ backgroundColor: '#DFD3C3', height: '89vh', borderTopRightRadius: 30 }}>
       <Box sx={{ position: 'absolute', top: '70px', left: '10px' }}>
@@ -24,12 +32,14 @@ export default function TeacherSidebar() {
           <ListItemButton
             key={item.name}
             onClick={() => {
+              handleClickMenu(item.name);
               navigate(item.link);
             }}
             sx={{ backgroundColor: `${item.bgcolor}`, mb: 2 }}
           >
             <img width="32px" alt="" src={item.icon} />
             <ListItemText sx={{ ml: 2 }}>{item.name}</ListItemText>
+            {isClicked[item.name] ? <Box width="20px" height="100%" backgroundColor={item.bgcolor} sx={{ position: 'absolute', left: '12.45vw' }} /> : null}
           </ListItemButton>
         ))}
       </List>
