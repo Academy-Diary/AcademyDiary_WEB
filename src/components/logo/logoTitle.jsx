c18dc4f6c1539592dd1e42ec336881debd467a85
godeka <godeka@naver.com>
2024-12-03 17:18:13 +0900

    design: 로고 타이틀 드래그 못하게 방지 #108

--- src/components/logo/logoTitle.jsx
+++ src/components/logo/logoTitle.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Typography } from '@mui/material';
 
+// -webkit-user-select:none;
+// -moz-user-select:none;
+// -ms-user-select:none;
+// user-select:none
 export default function LogoTitle({ mt, mb }) {
   return (
-    <Typography variant="h4" align="center" sx={{ mt, mb }}>
+    <Typography
+      variant="h4"
+      align="center"
+      sx={{
+        mt,
+        mb,
+        WebkitUserSelect: 'none',
+        MozUserSelect: 'none',
+        msUserSelect: 'none',
+        userSelect: 'none',
+      }}
+    >
       아카데미 다이어리
     </Typography>
   );
