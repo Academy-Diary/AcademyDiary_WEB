463d0610cded1020b3cc0b5cec9ed665d7b18a26
godeka <godeka@naver.com>
2024-12-03 12:53:32 +0900

    design: 시작 페이지 배경 추가 #108

--- src/pages/start/startPage.jsx
+++ src/pages/start/startPage.jsx
@@ -2,20 +2,45 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Container, Box } from '@mui/material';
+import Colors from '../../styles/colors';
 
 export default function StartPage() {
   return (
-    <Container component="main" maxWidth="xs">
+    <Box
+      sx={{
+        position: 'relative',
+        width: '100%',
+        height: '100vh',
+        backgroundColor: Colors.Beige,
+      }}
+    >
       <Box
         sx={{
-          paddingTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          width: '80%',
+          height: '95%',
+          position: 'absolute',
+          bottom: 0,
+          left: '50%',
+          transform: 'translateX(-50%)',
+          backgroundColor: Colors.White,
+          borderTopLeftRadius: 10,
+          borderTopRightRadius: 10,
         }}
-      >
-        <Outlet />
-      </Box>
-    </Container>
+      />
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            paddingTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            zIndex: 1, // 내용이 배경 위로 올라오도록 설정
+            position: 'relative', // zIndex를 적용하려면 필요
+          }}
+        >
+          <Outlet />
+        </Box>
+      </Container>
+    </Box>
   );
 }
