8fa65021b6e499c22e4b2a75b4764ac31930bc0b
godeka <godeka@naver.com>
2024-12-04 17:10:58 +0900

    design: 원장 페이지 배경 적용 #112

--- src/components/layouts/director.jsx
+++ src/components/layouts/director.jsx
@@ -4,6 +4,7 @@ import { Outlet } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import DirectorHeader from './header/directorHeader';
+import Colors from '../../styles/colors';
 
 /**
  *@description 원장화면 헤더 있는 레이아웃
@@ -17,10 +18,30 @@ import DirectorHeader from './header/directorHeader';
 
 export default function Director() {
   return (
-    <Box component="main">
+    <Box
+      component="main"
+      sx={{
+        width: '100%',
+        height: '100vh',
+        backgroundColor: Colors.Beige,
+      }}
+    >
       <DirectorHeader />
-      <Box sx={{ py: 10, px: 5, maxHeight: '100vh', overflow: 'auto' }}>
-        <Outlet />
+      <Box
+        sx={{
+          position: 'fixed',
+          width: '85%',
+          height: 'calc(100vh - 64px)',
+          top: '64px',
+          right: 0,
+          backgroundColor: Colors.White,
+          borderTopLeftRadius: 30,
+          overflow: 'auto',
+        }}
+      >
+        <Box sx={{ py: 3, px: 5, display: 'flex', flexDirection: 'column' }}>
+          <Outlet />
+        </Box>
       </Box>
     </Box>
   );
