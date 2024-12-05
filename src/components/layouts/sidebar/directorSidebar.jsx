cb6ff8a925ec92085cac78b7026d4b43d2317287
godeka <godeka@naver.com>
2024-12-06 00:43:56 +0900

    design: 사이드바 ListItemButton 높이, selected 색상 등 커스터마이징 #112

--- src/components/layouts/sidebar/directorSidebar.jsx
+++ src/components/layouts/sidebar/directorSidebar.jsx
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+import { styled } from '@mui/material/styles';
 import { Box, Collapse, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { CampaignRounded, CreditCardRounded, ExpandLess, ExpandMore, GroupsRounded, SchoolRounded } from '@mui/icons-material';
 
 import Colors from '../../../styles/colors';
+
+const StyledListItemButton = styled(ListItemButton)({
+  '&': {
+    height: 70,
+  },
+  '&:hover': {
+    backgroundColor: Colors.Brown10,
+  },
+  '&.Mui-selected': {
+    backgroundColor: Colors.Brown20,
+    '&:hover': {
+      backgroundColor: Colors.Brown30,
+    },
+  },
+});
 
 export default function DirectorSidebar() {
   const navigate = useNavigate();
@@ -34,108 +50,118 @@ export default function DirectorSidebar() {
       }}
     >
       <List component="nav">
-        <ListItemButton
+        <StyledListItemButton
           selected={pathname.startsWith('/director/manage-members') && !expand}
           onClick={() => {
             handleClickExpand();
           }}
+          divider
         >
           <ListItemIcon>
             <GroupsRounded />
           </ListItemIcon>
           <ListItemText primary="구성원 관리" />
           {expand ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
+        </StyledListItemButton>
         <Collapse in={expand} timeout="auto">
           <List>
-            <ListItemButton
+            <StyledListItemButton
               selected={pathname === '/director/manage-members/teachers'}
               onClick={() => {
                 navigate('/director/manage-members/teachers');
               }}
+              divider
             >
               <ListItemText primary="강사 관리" />
-            </ListItemButton>
-            <ListItemButton
+            </StyledListItemButton>
+            <StyledListItemButton
               selected={pathname === '/director/manage-members/students'}
               onClick={() => {
                 navigate('/director/manage-members/students');
               }}
+              divider
             >
               <ListItemText primary="학생 관리" />
-            </ListItemButton>
-            <ListItemButton
+            </StyledListItemButton>
+            <StyledListItemButton
               selected={pathname === '/director/manage-members/request-list'}
               onClick={() => {
                 navigate('/director/manage-members/request-list');
               }}
+              divider
             >
               <ListItemText primary="등록 요청 목록" />
-            </ListItemButton>
+            </StyledListItemButton>
           </List>
         </Collapse>
-        <ListItemButton
+        <StyledListItemButton
           selected={pathname.startsWith('/director/manage-courses')}
           onClick={() => {
             navigate('/director/manage-courses');
           }}
+          divider
         >
           <ListItemIcon>
             <SchoolRounded />
           </ListItemIcon>
           <ListItemText primary="강의 관리" />
-        </ListItemButton>
-        <ListItemButton
+        </StyledListItemButton>
+        <StyledListItemButton
           selected={pathname.startsWith('/director/tuition-fees') && !expand2}
           onClick={() => {
             handleClickExpand2();
           }}
+          divider
         >
           <ListItemIcon>
             <CreditCardRounded />
           </ListItemIcon>
           <ListItemText primary="학원비" />
           {expand2 ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
+        </StyledListItemButton>
         <Collapse in={expand2} timeout="auto">
           <List>
-            <ListItemButton
+            <StyledListItemButton
               selected={pathname === '/director/tuition-fees/payment-list'}
               onClick={() => {
                 navigate('/director/tuition-fees/payment-list');
               }}
+              divider
             >
               <ListItemText primary="학원비 납부 목록" />
-            </ListItemButton>
-            <ListItemButton
+            </StyledListItemButton>
+            <StyledListItemButton
               selected={pathname === '/director/tuition-fees/claim'}
               onClick={() => {
                 navigate('/director/tuition-fees/claim');
               }}
+              divider
             >
               <ListItemText primary="학원비 청구" />
-            </ListItemButton>
-            <ListItemButton
+            </StyledListItemButton>
+            <StyledListItemButton
               selected={pathname === '/director/tuition-fees/make-class'}
               onClick={() => {
                 navigate('/director/tuition-fees/make-class');
               }}
+              divider
             >
               <ListItemText primary="학원비 구성" />
-            </ListItemButton>
+            </StyledListItemButton>
           </List>
         </Collapse>
-        <ListItemButton
+        <StyledListItemButton
           selected={pathname.startsWith('/director/notice')}
           onClick={() => {
             navigate('/director/notice');
           }}
+          divider
         >
           <ListItemIcon>
             <CampaignRounded />
           </ListItemIcon>
           <ListItemText primary="전체공지" />
-        </ListItemButton>
+        </StyledListItemButton>
       </List>
     </Box>
   );
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -3,6 +3,9 @@ const Colors = {
   White: '#FFFFFF',
   Beige: '#EEEBDD',
   DarkBeige: '#DFD3C3',
+  Brown10: 'rgba(117, 83, 65, 0.1)', // 투명도 10
+  Brown20: 'rgba(117, 83, 65, 0.2)', // 투명도 20
+  Brown30: 'rgba(117, 83, 65, 0.3)', // 투명도 30
   Grey: '#5C5C5C',
 };
 
