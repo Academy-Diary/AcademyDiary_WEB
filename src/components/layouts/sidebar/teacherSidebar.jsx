46c8b5bcaa45dcb8121ff8866bf38d323a1208cc
godeka <godeka@naver.com>
2024-12-08 19:56:56 +0900

    design: 강사 사이드바 아이콘 material icon으로 변경 #108

--- src/components/layouts/sidebar/teacherSidebar.jsx
+++ src/components/layouts/sidebar/teacherSidebar.jsx
@@ -1,18 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Box, List, ListItemButton, ListItemText } from '@mui/material';
+import { Box, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { useLocation, useNavigate } from 'react-router-dom';
-import clip from '../../../img/Binder clip.png';
-import lectureIcon from '../../../img/lecture_icon.png';
-import consultationIcon from '../../../img/consultation_icon.png';
-import noticeIcon from '../../../img/notice_icon.png';
+import { CampaignRounded, QuestionAnswerRounded, SchoolRounded } from '@mui/icons-material';
 
 export default function TeacherSidebar() {
   const navigate = useNavigate();
   const { pathname } = useLocation();
   const items = [
-    { name: 'My 강의 목록', link: '/teacher/class', bgcolor: 'rgb(255, 211, 23, 0.6)', icon: lectureIcon },
-    { name: '학생 상담', link: '/teacher/counseling', bgcolor: 'rgb(6, 68, 32, 0.6)', icon: consultationIcon },
-    { name: '학원 공지', link: '/teacher/notice', bgcolor: 'rgb(2, 79, 81, 0.6)', icon: noticeIcon },
+    { name: 'My 강의 목록', link: '/teacher/class', bgcolor: 'rgb(255, 211, 23, 0.6)', icon: <SchoolRounded /> },
+    { name: '학생 상담', link: '/teacher/counseling', bgcolor: 'rgb(6, 68, 32, 0.6)', icon: <QuestionAnswerRounded /> },
+    { name: '학원 공지', link: '/teacher/notice', bgcolor: 'rgb(2, 79, 81, 0.6)', icon: <CampaignRounded /> },
   ];
   const originRef = useRef();
   const origin = originRef.current;
@@ -57,7 +54,7 @@ export default function TeacherSidebar() {
               onMouseOver={() => handleMenu(item.name)}
               sx={{ mb: 2, padding: 2, bgcolor: 'rgb(202, 185, 170)' }}
             >
-              <img width="32px" alt="" src={item.icon} />
+              <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText sx={{ ml: 2 }}>{item.name}</ListItemText>
             </ListItemButton>
           ) : (
@@ -70,7 +67,7 @@ export default function TeacherSidebar() {
               onMouseOver={() => handleMenu(item.name)}
               sx={{ mb: 2, padding: 2 }}
             >
-              <img width="32px" alt="" src={item.icon} />
+              <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText sx={{ ml: 2 }}>{item.name}</ListItemText>
             </ListItemButton>
           )
